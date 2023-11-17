16c58b79e1ed070c49327a8561f6df800202cce4
Igor Pessoa <igor.silva26@outlook.com>
2023-11-16 22:05:01 -0300

    finished dinamic theme changes

---
+++ public/js/script.js
@@ -0,0 +1,42 @@
+function changeTheme() {
+    const theme = localStorage.getItem('theme')
+    const body = document.querySelector('body')
+    const button = document.querySelector('.theme-button')
+
+    if (theme) {
+        let newTheme
+
+        if (theme === 'light') {
+            newTheme = 'dark'
+            button.innerHTML = `<img src="/images/sun-icon.png" alt="sun icon">`
+            body.classList.remove('light')
+            body.classList.add('dark')
+        } else {
+            newTheme = 'light'
+            button.innerHTML = `<img src="/images/moon-icon.png" alt="moon icon">`
+            body.classList.remove('dark')
+            body.classList.add('light')
+        }
+
+        localStorage.setItem('theme', newTheme)
+        return
+    }
+
+    localStorage.setItem('theme', 'dark')
+    body.classList.add('dark')
+}
+
+function verifyTheme() {
+    const theme = localStorage.getItem('theme')
+    const body = document.querySelector('body')
+
+    if (theme == 'dark') {
+        body.classList.add('dark')
+        button.innerHTML = `<img src="/images/sun-icon.png" alt="sun icon">`
+    } else {
+        body.classList.add('light')
+        button.innerHTML = `<img src="/images/moon-icon.png" alt="moon icon">`
+    }
+}
+
+verifyTheme()
